82483dcfdada7ca66a66974636038d6c87ed24ba
Anthony Fort <ajf2195@columbia.edu>
2023-11-20 22:28:14 +0000

    Begins contact form'

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,19 @@
 import { useState } from 'react'
-// import emailjs from 'emailjs-com'
+import emailjs from '@emailjs/browser'
 
 export default function Contact () {
+
   
+  return (
+    <div className='contact-container'>
+      <h2 className='standard-text-header'>CONTACT</h2>
+      <p className='contact-blurb'>Need a website? Let's help the world discover the real you...</p>
+      <form className='contact-form'>
+        <input type='text' name='name' placeholder='Your name' className='form-field'></input>
+        <input type='email' name='email' placeholder='Your email' className='form-field'></input>
+        <textarea type='textarea' name='message' placeholder='Your message' className='form-field' rows={5}></textarea>
+        <input type="submit" className='contact-submit' />
+      </form>
+    </div>
+  )
 }
