09a38180ad64a007cb9792b24b949478f7dd0925
Anthony Fort <ajf2195@columbia.edu>
2023-11-20 22:04:06 +0000

    Adds coding and music category pages

--- src/components/Coding.jsx
+++ src/components/Coding.jsx
@@ -1,3 +1,24 @@
+import { Link } from 'react-router-dom'
+
 export default function Coding () {
-  
+
+  const projects = [
+    'CM Database',
+    'Froggus Ad Parnassum',
+    'AAG Music',
+    'AFJH Store',
+  ]
+
+  return (
+    <div className="standard-category-div">
+      <h2 className='standard-text-header'>CODING</h2>
+      <ul className="standard-category-list">
+        {projects.map(project => (
+          <li key={project} className="standard-category-li">
+            <Link style={{color: '#A7A7A7'}}>{project}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
--- src/components/Music.jsx
+++ src/components/Music.jsx
@@ -1,3 +1,51 @@
+import { Link } from 'react-router-dom'
+
 export default function Music () {
-  
+  const musicWork = [
+    {
+      title: "PhD Research",
+      id: 0,
+    },
+    {
+      title: "Musical 'Decoding'",
+      id: 1,
+    },
+    {
+      title: "Fun Kid's Stuff",
+      id: 2,
+    },
+    {
+      title: "Podcast",
+      id: 3,
+    },
+    {
+      title: "The 'Even-Note' Illusion",
+      id: 4,
+    },
+    {
+      title: "Songwriting",
+      id: 5,
+    },
+    {
+      title: "University Teaching",
+      id: 6,
+    },
+    {
+      title: "Union Chapel",
+      id: 7,
+    },
+  ]
+
+  return (
+    <div className="standard-category-div">
+      <h2 className='standard-text-header'>MUSIC</h2>
+      <ul className="standard-category-list">
+        {musicWork.map(item => (
+          <li key={item.id} className="standard-category-li">
+            <Link style={{color: '#A7A7A7'}}>{item.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
