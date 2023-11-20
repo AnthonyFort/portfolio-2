2d03ee14edbaffb235fab1f2d1350a2ba35f33d8
Anthony Fort <ajf2195@columbia.edu>
2023-11-20 22:53:02 +0000

    Adds functionality to email form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,16 +3,49 @@ import emailjs from '@emailjs/browser'
 
 export default function Contact () {
 
+  const [isSubmitting, setIsSubmitting] = useState(false)
+  const [stateMessage, setStateMessage] = useState(null)
+
+  const sendEmail = (e) => {
+    e.persist()
+    e.preventDefault()
+    setIsSubmitting(true)
+    emailjs
+      .sendForm(
+        import.meta.env.VITE_EMAIL_SERVICE_ID,
+        import.meta.env.VITE_EMAIL_TEMPLATE_ID,
+        e.target,
+        import.meta.env.VITE_EMAIL_PUBLIC_KEY
+      )
+      .then(
+        (result) => {
+          setStateMessage('Message sent!')
+          setIsSubmitting(false)
+          setTimeout(() => {
+            setStateMessage(null)
+          }, 5000)
+        },
+        (error) => {
+          setStateMessage('Something went wrong, please try again later')
+          setIsSubmitting(false)
+          setTimeout(() => {
+            setStateMessage(null)
+          }, 5000)
+        }
+      )
+    e.target.reset()  
+  }
   
   return (
     <div className='contact-container'>
       <h2 className='standard-text-header'>CONTACT</h2>
       <p className='contact-blurb'>Need a website? Let's help the world discover the real you...</p>
-      <form className='contact-form'>
-        <input type='text' name='name' placeholder='Your name' className='form-field'></input>
-        <input type='email' name='email' placeholder='Your email' className='form-field'></input>
+      <form className='contact-form' onSubmit={sendEmail}>
+        <input type='text' name='user_name' placeholder='Your name' className='form-field'></input>
+        <input type='email' name='user_email' placeholder='Your email' className='form-field'></input>
         <textarea type='textarea' name='message' placeholder='Your message' className='form-field' rows={5}></textarea>
         <input type="submit" className='contact-submit' />
+        {stateMessage && <p>{stateMessage}</p>}
       </form>
     </div>
   )
