713645c886ea4e5d3135847fe1d7f8fa6abe98f4
Anthony Fort <ajf2195@columbia.edu>
2023-11-20 14:25:03 +0000

    Imports components into app js

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,24 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
 
+import About from './components/About'
+import Coding from './components/Coding'
+import Contact from './components/Contact'
+import Music from './components/Music'
+import Nav from './components/Nav'
+import NotFound from './components/NotFound'
+
 function App() {
 
   return (
     <>
+      <Nav />
       <Routes>
-        <Route path='/'/>
+        <Route path='/' element={<About />} />
+        <Route path='/coding' element={<Coding />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/Music' element={<Music />} />
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </>
   )
